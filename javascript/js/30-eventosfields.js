2341443291c1cb5c2d375430f4a83759a517129c
monicagaitanrivera <riveramonny123@gmail.com>
2022-02-11 08:08:19 -0500

    subiendo cambios

--- javascript/js/30-eventosfields.js
+++ javascript/js/30-eventosfields.js
@@ -1,1 +1,15 @@
 "use strict"
+
+function validarInfo(numeroDocumento) {
+    let numDocumento = document.getElementById(numeroDocumento).value ;
+
+    if (numDocumento === "") {
+        alert ("El numero del documento es un dato obligatorio");
+    }
+            else {
+                alert ("La informacion ha sido enviada con exito")
+            }
+        }
+        
+       
+
