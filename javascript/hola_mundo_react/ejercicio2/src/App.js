605699861d45e55743b797b432a824e836d3a8b6
monicagaitanrivera <riveramonny123@gmail.com>
2022-05-24 11:47:20 -0500

    mundo react

---
+++ javascript/hola_mundo_react/ejercicio2/src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return(
+      <div className='App'>
+        <h2>!!!!!Hola mundo React!!!</h2>
+      </div>
+    )
+  }
+}
+export default App;
